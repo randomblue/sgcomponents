007c59d1dbad448571e026b7b7828e729e544190
randomblue <jpmedallon@gmail.com>
2022-01-09 16:37:32 -0800

    added all props into parent removed from child

--- src/index.js
+++ src/index.js
@@ -6,10 +6,26 @@ import CommentDetail from './CommentDetail';
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail author="JP"/>
-            <CommentDetail author="Bo"/>
-            <CommentDetail author="Sherwin"/>
-            <CommentDetail author="Tenzin"/>
+            <CommentDetail 
+                avatarPhoto={faker.image.image()} 
+                author="JP" 
+                timeAgo="Today at 4:45PM" 
+                blogComment="Nice"/>
+            <CommentDetail 
+                avatarPhoto={faker.image.image()} 
+                author="Bo" 
+                timeAgo="Today at 1:45PM" 
+                blogComment="Truck"/>
+            <CommentDetail 
+                avatarPhoto={faker.image.image()} 
+                author="Sherwin" 
+                timeAgo="Yesterday at 4:45AM" 
+                blogComment="Bro"/>
+            <CommentDetail 
+                avatarPhoto={faker.image.image()} 
+                author="Tenzin" 
+                timeAgo="Yesterday at 6:69PM" 
+                blogComment="LOL"/>
         </div>
     );
 };
