d9a6f266197f5e72e3db67756c84b68ad2798fb4
randomblue <jpmedallon@gmail.com>
2022-01-09 16:02:03 -0800

    add prop author

--- src/index.js
+++ src/index.js
@@ -6,7 +6,10 @@ import CommentDetail from './CommentDetail';
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail />
+            <CommentDetail author="JP"/>
+            <CommentDetail author="Bo"/>
+            <CommentDetail author="Sherwin"/>
+            <CommentDetail author="Tenzin"/>
         </div>
     );
 };
